0b7a4d6f48f48b981fb6964f7bb3cabab8f5fc29
mikhail <shiningproxima@gmail.com>
2019-03-12 21:24:35 +0300

    gui_app.py
    button_action attached to button1 at init via command
    action outputs passed args(which are empty tuple)

--- gui_app.py
+++ gui_app.py
@@ -1,6 +1,9 @@
 from tkinter import *
 
+def button_action(*args):
+	print(len(args), args)
+
 root = Tk()
-button1 = Button(root, text = "what do i do?")
+button1 = Button(root, text = "what do i do?", command = button_action)
 button1.pack()
 root.mainloop()
